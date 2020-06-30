9b31b7db3379a310fa815a7639a8df866ed5cc83
zhaorj <zhaorj@neusoft.com>
2020-06-30 16:55:50 +0800

    add example

--- lib/unp.h
+++ lib/unp.h
@@ -3,13 +3,33 @@
 #include "config.h"
 
 extern "C" {
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>	/* inet(3) functions */
+#include <sys/un.h>
 
 }
 
 /* Following shortens all the typecasts of pointer arguments: */
 #define	SA	struct sockaddr
+
+/* Miscellaneous constants */
+#define	MAXLINE		4096	/* max text line length */
+#define	BUFFSIZE	8192	/* buffer size for reads and writes */
+
+namespace  unp
+{
+    void err_ret(const char *fmt, ...);
+    void err_sys(const char *fmt, ...);
+    void err_dump(const char *fmt, ...);
+    void err_msg(const char *fmt, ...);
+    void err_quit(const char *fmt, ...);
+    char *Sock_ntop(const struct sockaddr *sa, socklen_t salen);
+}
 
 #endif
